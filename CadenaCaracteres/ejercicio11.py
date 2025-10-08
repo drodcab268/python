f7e367f0a29239ed93c7453b8548f33811b1328e
Daniel Rodríguez (IZV) <danielroca26@gamil.com>
2025-10-08 10:25:10 +0200

    ejercicio11.py sin terminar

---
+++ CadenaCaracteres/ejercicio11.py
@@ -0,0 +1,7 @@
+produ=input("Inroduzca el nombre del producto: ")
+preci=input("Introduzca el precio del producto: ")
+numUni=input("Introduzca el número de unidades: ")
+
+total=float(preci)*float(numUni)
+
+print(f"Producto: {produ} Precio: {preci}€, Unidades: {numUni}, Total: {total}€")
