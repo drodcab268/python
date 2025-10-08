ce29161049bed0b73e4fe4a4084d96d2f16e4259
Daniel Rodríguez (IZV) <danielroca26@gamil.com>
2025-10-08 09:50:16 +0200

    ejercicio9.py corrección del input

--- CadenaCaracteres/ejercicio9.py
+++ CadenaCaracteres/ejercicio9.py
@@ -1,3 +1,3 @@
-fecha=input('Introduzca la fecha de hoy con el siguiente formato "dd/mm/aaaa": ')
+fecha=input('Introduzca la fecha de su nacimiento con el siguiente formato "dd/mm/aaaa": ')
 fechaSplit=fecha.split("/")
-print(f"Hoy es día {fechaSplit[0]}, del mes {fechaSplit[1]}, del año {fechaSplit[2]}")
+print(f"Usted nació el día {fechaSplit[0]}, del mes {fechaSplit[1]}, del año {fechaSplit[2]}")
