78254fd8b3594f19e009adfbf782ee25bec15c45
Daniel Rodríguez (IZV) <danielroca26@gamil.com>
2025-10-14 13:44:00 +0200

    ejercicio2.py mejor formato

--- Condicionales/ejercicio2.py
+++ Condicionales/ejercicio2.py
@@ -1,7 +1,7 @@
-contra = input("Introduzca su contrasña: ").lower()
-contGuard = ("holaCaracola").lower()
+contra = input("Introduzca su contrasña: ")
+contGuard = ("holaCaracola")
 
-if contra == contGuard:
+if contra.lower() == contGuard.lower():
     print ("La contraseña es correcta, accediendo...")
 else:
-    print ("La contraseña es incorrecta, pruebe de nuevo...")
+    print ("La contraseña es incorrecta, pruebe de nuevo!")
