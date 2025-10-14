7e310026a19800957a323957cb4c33877cee723e
Daniel Rodríguez (IZV) <danielroca26@gamil.com>
2025-10-14 13:41:00 +0200

    ejercicio2.py

---
+++ Condicionales/ejercicio2.py
@@ -0,0 +1,7 @@
+contra = input("Introduzca su contrasña: ").lower()
+contGuard = ("holaCaracola").lower()
+
+if contra == contGuard:
+    print ("La contraseña es correcta, accediendo...")
+else:
+    print ("La contraseña es incorrecta, pruebe de nuevo...")
