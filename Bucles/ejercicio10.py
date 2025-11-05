292171a153c37961a1256bb66591d124329004f3
drodcab268 <danielroca26@gmail.com>
2025-11-05 10:17:39 +0100

    ejercicio10.py

--- Bucles/ejercicio10.py
+++ Bucles/ejercicio10.py
@@ -1,9 +1,10 @@
 num=int(input("Introduzca un número entero positivo: "))
 
 for x in range(1,num+1):
-    print(x, end=",")
+    print(f"{num} / {x} = ", (num%x))
     if (num % x == 0 and x != num and x != 1):
         print(f"El número {num} no es primo")
+        break
     else:
         print(f"El número {num} es primo")
         
