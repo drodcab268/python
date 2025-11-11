174ca98e07e603ee98711762a9500e3a8dc6a483
drodcab268 <danielroca26@gmail.com>
2025-11-11 13:54:12 +0100

    ejercicio7.py revisión

--- Bucles/ejercicio7.py
+++ Bucles/ejercicio7.py
@@ -1,5 +1,4 @@
-num=int(input("Introduzca un número entero positivo: "))
-
 for x in range(1,11):
-
-    print(f"{num} x {x} = ",(num*x))
+    for y in range(1,11):
+        print(f"{x} x {y} = ",(x*y))
+    print()
