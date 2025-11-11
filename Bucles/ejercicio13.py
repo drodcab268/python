86743254ba9a4e3a81d2abc7c18a42360a44d107
drodcab268 <danielroca26@gmail.com>
2025-11-11 14:13:55 +0100

    ejercicio13.py

--- Bucles/ejercicio13.py
+++ Bucles/ejercicio13.py
@@ -1,4 +1,11 @@
 todo=input('Escriba todo lo que quiera ("salir" para parar): ')
-arrayTodo[0]=todo
+arrayTodo=[]
+arrayTodo.insert(0,todo)
+count=1
 while todo != "salir":
     todo=input('Escriba todo lo que quiera ("salir" para parar): ')
+    arrayTodo.insert(count,todo)
+    count=count+1
+
+for x in arrayTodo:
+    print(x)
