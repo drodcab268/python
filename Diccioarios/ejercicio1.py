a7229e2f6bb748b4df3f9c7b954df728643b0b9e
drodcab268 <danielroca26@gmail.com>
2026-01-14 10:17:42 +0100

    ejercicio1.py corregido

--- Diccioarios/ejercicio1.py
+++ Diccioarios/ejercicio1.py
@@ -1,3 +1,3 @@
-divisas={'Euro':'€','Dollar':'$','Yen':'¥','Libra':'£','Sol':'S/'}
+divisas={"Euro":"€","Dollar":"$","Yen":"¥","Libra":"£","Sol":"S/"}
 div=input("Introduzca una divisa: ")
 print (f"El simbolo de la divisa {div} es: {divisas[div]}")
