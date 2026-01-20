32c5ba265172cf82a67dc4b5bc90176abed5a8d6
drodcab268 <danielroca26@gmail.com>
2026-01-20 13:12:28 +0100

    ejercicio2 terminado

--- Diccioarios/ejercicio2.py
+++ Diccioarios/ejercicio2.py
@@ -1,3 +1,6 @@
-datos=input("Introduzca su nombre, edad, dirección y número de teléfono: ")
-
-formulario={""}
+nombre=input("Introduzca su nombre: ")
+edad=input("Introduzca su edad: ")
+direccion=input("Introduzca su dirección: ")
+telefono=input("Introduzca su nº de teléfono: ")
+persona={"nombre": nombre,"edad": edad,"direccion": direccion,"telefono": telefono}
+print(persona["nombre"], "tiene", persona["edad"], "años, vive en", persona["direccion"], "y su número de teléfono es", persona["telefono"])
