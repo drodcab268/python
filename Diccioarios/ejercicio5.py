1145db34c48b70f32f743a7b4861f4adbfe98fb2
drodcab268 <danielroca26@gmail.com>
2026-02-02 12:50:03 +0100

    ejercicio5.py terminado

--- Diccioarios/ejercicio5.py
+++ Diccioarios/ejercicio5.py
@@ -1,8 +1,8 @@
 curso={"Matemáticas": 6, "Física": 4, "Química": 5}
-total_creditos = 0
+total_cred = 0
 
-for asignatura, creditos in curso.items():
-    print(asignatura, "tiene", creditos, "créditos")
-    total_creditos += creditos
+for asig, cred in curso.items():
+    print(asig, " tiene: ", cred, " créditos")
+    total_cred += cred
     
-print("Número total de créditos del curso: ", total_creditos)
+print("Número total de créditos del curso: ", total_cred)
